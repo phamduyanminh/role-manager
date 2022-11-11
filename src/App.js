605473fa076ffbcfaa496d7a1a570f7137eedeed
phamduyanminh <phamduyanminh3120@gmail.com>
2022-11-10 21:38:07 -0500

    Created fundamental logic and layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState } from 'react';
+
 import './App.css';
+import Employee from './components/Employees';
 
 function App() {
+  const imgURL = "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+
+  const[employees, setEmployees] = useState([
+    {
+      name: "Minh",
+      role: "intern",
+      image: imgURL
+    },
+    {
+      name: "Minh2",
+      role: "intern",
+      image: imgURL
+    },
+    {
+      name: "Minh3",
+      role: "intern",
+      image: imgURL
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text"/>
+      <div className="flex flex-wrap justify-center">
+        {employees.map((employee) => {
+          return(
+            <Employee key={employee.name} name={employee.name} role={employee.role} image={employee.image}/>
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/components/Employees.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import EditEmployee from './EditEmployee';
+
+const Employee = (props) => {
+    return(
+        <div className="min-w-[350px] max-w-[350px] py-8 px-8 max-w-sm m-2 bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+            <img className="object-cover rounded-full h-[100px] w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src={props.image}/>
+            <div className="text-center space-y-2 sm:text-left">
+                <div className="space-y-0.5">
+                    <p className="text-lg text-black font-semibold">
+                        {props.name}
+                    </p>
+                    <p className="text-slate-500 font-medium">
+                        {props.role}
+                    </p>
+                </div>
+                <EditEmployee name={props.name} role={props.role}/>
+            </div>
+        </div>
+    );
+}
+
+export default Employee;
